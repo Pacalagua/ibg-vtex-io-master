05378ade53caf93918257c20f7a1cdd00c61352e
Pacalagua <jonathan.pacalagua@ibg.com.co>
2023-04-14 16:09:49 -0500

    Se remueve infografía de compra con Addi

--- react/components/BancoBogotaPDP/BancoBogotaPDP.jsx
+++ react/components/BancoBogotaPDP/BancoBogotaPDP.jsx
@@ -3,17 +3,19 @@ import styles from './styles.css'
 
 const BancoBogotaPDP = () => {
   return (
-    <div className={styles.contenedor__pp}>
-      <div className={styles.cont__superintendencia}>
-           <img src="https://ibgcol.vteximg.com.br/arquivos/superintendencia-financiera.svg" alt="superintendencia" />
+    <>
+      <div className={styles.contenedor__pp}>
+        <div className={styles.cont__superintendencia}>
+          <img src="https://ibgcol.vteximg.com.br/arquivos/superintendencia-financiera.svg" alt="superintendencia" />
+        </div>
+        <div className={styles.contenedor__logo}>
+          <img src="https://ibgcol.vteximg.com.br/arquivos/logo-banco-bogota.svg" alt="logo banco de bogota" />
+        </div>
+        <div className={styles.texto__bancob}>
+          Llévatelo con Tarjeta de Crédito Banco de Bogotá. Solicita <a href="https://digital.bancodebogota.com/tarjeta-credito/digital/index.html?utm_source=ibg&utm_medium=referral&utm_campaign=pc_banner_ibg&utm_content=home" target="banco_bogota">Aquí</a>
+        </div>
       </div>
-      <div className={styles.contenedor__logo}>
-        <img src="https://ibgcol.vteximg.com.br/arquivos/logo-banco-bogota.svg" alt="logo banco de bogota" />
-      </div>
-      <div className={styles.texto__bancob}>
-        Llévatelo con Tarjeta de Crédito Banco de Bogotá. Solicita <a href="https://digital.bancodebogota.com/tarjeta-credito/digital/index.html?utm_source=ibg&utm_medium=referral&utm_campaign=pc_banner_ibg&utm_content=home" target="banco_bogota">Aquí</a>
-      </div>
-    </div>
+    </>
   )
 }
 
--- react/components/SumasPay/SumasPay.jsx
+++ react/components/SumasPay/SumasPay.jsx
@@ -3,14 +3,16 @@ import styles from './styles.css'
 
 const SumasPay = () => {
   return (
-    <div className={styles.contenedor__sumas}>
-      <div className={styles.contenedor__logosumas}>
-        <img src="https://ibgcol.vteximg.com.br/arquivos/ids/158019/sumaspay-checkout-ibg-28x28.png" alt="logo Su+Pay" />
+    <>
+      <div className={styles.contenedor__sumas}>
+        <div className={styles.contenedor__logosumas}>
+          <img src="https://ibgcol.vteximg.com.br/arquivos/ids/158019/sumaspay-checkout-ibg-28x28.png" alt="logo Su+Pay" />
+        </div>
+        <div className={styles.texto__sumas}>
+          Solicita tu Crédito SU+ Pay 100% digital en menos de 10 minutos. <a href="/formas-de-pago" target="_blank">Saber más</a>
+        </div>
       </div>
-      <div className={styles.texto__sumas}>
-      Solicita tu Crédito SU+ Pay 100% digital en menos de 10 minutos. <a href="/formas-de-pago" target="_blank">Saber más</a>
-      </div>
-    </div>
+    </>
   )
 }
 
