4d8ed4b38736ae4968416f10d79ea97962e3a221
Jonastat <jpacalaguam@uqvirtual.edu.co>
2023-09-14 15:22:01 -0500

    Se añade botón flotante de WhatsApp

--- react/components/BtnWp/BtnWhatsapp.js
+++ react/components/BtnWp/BtnWhatsapp.js
@@ -1,4 +1,4 @@
-/* import React, { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import style from './style.css'
 
 
@@ -6,8 +6,7 @@ export const BtnWhatsapp = () => {
 
     return (
         <div className={style.btnWp}>
-            <a href="https://api.whatsapp.com/send?phone=573173729437" target="_blank" className="link-ayuda-header"><img className={style.btnIMG} src="https://ibgcol.vteximg.com.br/arquivos/icon-whastapp.png" /></a>
+            <a href="https://api.whatsapp.com/send?phone=573173729437" target="_blank" className="link-ayuda-header"><img className={style.btnIMG} src="https://ibgcol.vtexassets.com/arquivos/ids/158589/icon-whatsapp.svg" /></a>
         </div>
     )
 }
- */
