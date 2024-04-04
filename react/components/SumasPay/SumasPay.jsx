07faf940775c1d9c7a3e79910735dcfbdbcfdb32
Jonastat <jpacalaguam@uqvirtual.edu.co>
2024-04-04 12:39:07 -0500

    Se añaden cambios solicitados por Sumas Pay en landing info formas de pago, pdp y checkout

--- react/components/SumasPay/SumasPay.jsx
+++ react/components/SumasPay/SumasPay.jsx
@@ -6,10 +6,10 @@ const SumasPay = () => {
     <>
       <div className={styles.contenedor__sumas}>
         <div className={styles.contenedor__logosumas}>
-          <img src="https://ibgcol.vteximg.com.br/arquivos/ids/158019/sumaspay-checkout-ibg-28x28.png" alt="logo Su+Pay" />
+          <img src="https://ibgcol.vteximg.com.br/arquivos/ids/159097/AF_FLAG_Isotipo_Sumas_pay_-_checkout_2024.svg" alt="logo Su+Pay" />
         </div>
         <div className={styles.texto__sumas}>
-          Solicita tu Crédito SU+ Pay 100% digital en menos de 10 minutos. <a href="/formas-de-pago" target="_blank">Saber más</a>
+          Solicita tu Crédito Sumas Pay 100% digital en menos de 10 minutos. <a href="/formas-de-pago" target="_blank">Saber más</a>
         </div>
       </div>
     </>
